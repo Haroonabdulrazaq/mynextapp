e06f70c6ea04294ac1d7c4a77cfd2c2edb828871
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2022-10-09 22:34:16 +0100

    Add Dynamic route

---
+++ pages/products/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProductlList = () => {
+  return (
+    <>
+      <h2>ProductlList Page</h2>
+      <h2>ProductlList Page</h2>
+      <h2>ProductlList Page</h2>
+    </>
+  )
+}
+
+export default ProductlList
