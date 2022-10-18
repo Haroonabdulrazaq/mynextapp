1acba5f7c2403e4587fc0b9a618a04900d2cef82
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2022-10-18 19:22:26 +0100

    Update db.json

--- pages/news/[categories]/index.js
+++ pages/news/[categories]/index.js
@@ -7,7 +7,7 @@ const Category = ({categories}) => {
 
   return (
     <>
-      <div>Category</div>
+      <h1>Category</h1>
       <div>
         {
           categories.map((category)=>(
@@ -15,6 +15,7 @@ const Category = ({categories}) => {
               <div>
                 <h2>{category.id}. {category.title} </h2>
                 <p>{category.description}</p>
+                <hr/>
               </div>
             </Link>
           ))
@@ -28,9 +29,8 @@ export default Category
 
 export async function getServerSideProps(context){
   const {params} = context
-  const response = await fetch(`http://localhost:4000/news`)
-  const articles = await response.json()
-  const data = articles.filter((article)=>  article.category === params.categories)
+  const response = await fetch(`http://localhost:4000/news?category=${params.categories}`)
+  const data = await response.json()
 
   return {
     props: {
--- pages/profile.js
+++ pages/profile.js
@@ -4,6 +4,6 @@ const Profile = () => {
   return (
     <h1>Profile Page</h1>
   )
-}
+} 
 
 export default Profile
