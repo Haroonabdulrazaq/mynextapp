a21bf18bfb553b2be2db7a4ab662665f8c15fe19
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2022-10-19 14:53:59 +0100

    Done with SSR

--- pages/events.js
+++ pages/events.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import List from 'next/link'
+import { useRouter } from 'next/router'
 
-const EventList = ({eventList}) => {
+const EventList = ({eventList, queryString}) => {
+  const router = useRouter()
+  const [event, setEvent] = useState(eventList)
+
+  const fetchSportEvent = async() =>{
+    const response = await fetch('http://localhost:4000/events?category=sports')
+    const data = await response.json()
+    setEvent(data)
+
+    router.push(`/events?category=sports`, undefined, {shallow: true})
+  }
+
   return (
     <>
+      <button onClick={fetchSportEvent}>Sport Event</button>
       <h2>List of Events</h2>
-      {eventList.map((event)=>(
+      {event.map((event)=>(
           <List key={event.id} href={`/events/${event.id}`}>
             <div>
               <h2> {event.id}. {event.title} - {event.date} | {event.category} </h2>
@@ -20,13 +33,18 @@ const EventList = ({eventList}) => {
 
 export default EventList
 
-export async function getServerSideProps(){
-  const response = await fetch('http://localhost:4000/events')
+export async function getServerSideProps(context){
+  const {query} = context
+  const {category} = query
+  const queryString = category? 'category=sports' : ''
+
+  const response = await fetch(`http://localhost:4000/events?${queryString}`)
   const data = await response.json()
 
   return {
     props: {
-      eventList: data
+      eventList: data,
+      queryString
     }
   }
 }
