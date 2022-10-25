7ded43a96d4603c2783409b7cf69bbdb567eae63
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2022-10-25 22:07:34 +0100

    Add path aliases

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
-import '../styles/globals.css'
+import 'styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/layout.css'
+import 'styles/layout.css'
 import Head from 'next/head';
 
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Header from '@/layout/Header';
+import Footer from '@/layout/Footer';
 
 function MyApp({ Component, pageProps }) {
   if(Component.getLayout){
--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
-import Header from '../components/Header'
+import Header from '../components/layout/Header'
 
 const About = () => {
   return (
